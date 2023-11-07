919ed5dc14d62c49e224efd1ee34cde22c14e458
Laboratorio <laboratorio@udla-ec.int>
2023-11-07 15:19:02 -0500

    Final de ejercicio

--- Arreglos.c
+++ Arreglos.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int arg, char const argv[])
+#include<time.h>
+int main(int arg,int a, char const argv[])
 {
     srand(time(NULL));
-    int vector1[10];
-    printf("\nIngrese 10 datos");
-for(int i = 0; i < 10; i++)
+    int vector1[20];
+    printf("\nSus 20 numeros son: ");
+for(int i = 0; i < 20; i++)
 {
-    //scanf("%d",&vector1[i]);
-    vector1[i]=rand()%59 +25;
+    vector1[i]=rand()%10 +1;
 }
     printf("\n");
-for(int i = 0; i < 10; i++)
+for(int i = 0; i < 20; i++)
 {
     printf("%d \n", vector1[i]);
 }
+    printf("\nIngrese uno de los numeros genrados: ");
+    scanf("%d", &a);
+    int contador = 0;
+for(int i = 0; i<20; i++)
+{
+    if (vector1[i] == a){
+    contador++;
+    }
+}
+printf("el numero se repite: %d", contador);
 return 0;
 }
