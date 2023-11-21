815e0fb08f414605eb89020f4a3427464dc25616
Laboratorio <laboratorio@udla-ec.int>
2023-11-21 13:48:56 -0500

    asdadasda

--- Prueba.c
+++ Prueba.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main (){
     printf("Hola Mundo");
+    printf("Hola Mundo");
+    printf("Hola Mundo");
+    printf("Hola Mundo");
+    printf("Hola Mundo");
+    
     return 0;
 }
