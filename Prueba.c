6d6744369a229829d08f54010eba8e4161d6d765
Jorge <jorge.ramos.vasquez@udla.edu.ec>
2023-11-21 14:45:46 -0500

    Inicio y final

--- Prueba.c
+++ Prueba.c
@@ -1,20 +1,33 @@
 #include<stdio.h>
-int main (){
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundo");
-    printf("Hola Mundodasdsasdadadsadadsadadsa");
-    printf("Hola Mundodasdsasdadadsadadsadadsa");
-    printf("Hola Mundodasdsasdadadsadadsadad7245454554sa");
+#include<stdlib.h>
+#include<time.h>
 
+int main()
+{
+    int n, m;
+    printf("Ingrese el numero de filas (m): ");
+    scanf("%d", &m);
+    printf("Ingrese el numero de columnas (n): ");
+    scanf("%d", &n);
 
+    int matrix[m][n]; 
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i][j] = 0; 
+        }
+    }
+    printf("Matriz:\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
